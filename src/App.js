477584f9204e97a8cdcb9af9eb20ceecb985f5fc
zeeshansyed695 <zeeshansyed695@users.noreply.github.com>
2021-03-27 18:09:34 +0500

    Update App.js
    React classes and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import './App.css';
+import ReactDom from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World from all of us!</h1>      
-    </div>
-  );
+class Car extends React.Component{
+  constructor(){
+    super();
+    this.state = {color:'red'} 
+  }
+  render(){
+    return <div>
+          <h1>THis is my car </h1>
+          <h2>Car color is {this.props.brand}</h2>
+          
+          </div> 
+  }
 }
-
-export default App;
+class Garage extends React.Component{
+  render(){
+    return <h2>This is my garage of cars and jeep collection</h2>
+  }
+}
+class Jeep extends React.Component{
+  render(){
+    return <div>
+      <Car brand = "blue" />
+     <h1>I am ajeep on rally which color is not {this.props.brand} </h1>
+     <Garage />
+     
+     </div>
+  }
+}
+ReactDom.render(<Jeep brand = "blue" />,document.getElementById('root'));
+export default Jeep;
